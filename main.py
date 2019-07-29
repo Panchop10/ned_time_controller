337a2d967ae2ce4b2758545f0a107ee0f3cbf963
Panchop <folivares@arenasoft.cl>
2019-07-29 11:06:51 +0100

    fist test with nfc reader and python

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-  print('Hello World!')
+import os
+
+read_card = 'nfc-mfultralight r mycardUltra.mfd'
+os.system(read_card)
+
+card_to_hex = 'xxd mycardUltra.mfd > mycardUltra.hex' 
+os.system(card_to_hex)
+
+
