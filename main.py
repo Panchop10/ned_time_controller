d5f05f170f9b98e713ad9e796db56038427409d8
ZeynepCAN-software <zeynepcan96@hotmail.com>
2019-07-31 12:12:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ os.system(read_card)
 
 card_to_hex = 'xxd mycardUltra.mfd > mycardUltra.hex'
 os.system(card_to_hex)
-
-
-
-id_num_hex=card_to_hex[:19]
+f=open("mycardUltra.hex","r")
+first=f.readline()
+Line=f.readline()
+id_num_hex=line.substring(9:17)
 print(id_num_hex)
-# id_num_hex1=id_num_hex.replace(' ', '').rstrip('0')
-#
-# if (len(id_num_hex1))%2 == 1:
-#     id_num=id_num_hex1 + "0"
-# os.system(id_num)
-#
-#
-#
-# id_num = codecs.decode(id_num, 'hex').decode('ascii')
-#
+
+
+
+
+#id_num_hex1=id_num_hex.replace(' ', '').rstrip('0')
+
+#if (len(id_num_hex1))%2 == 1:
+    id_num=id_num_hex1 + "0"
+#os.system(id_num)
+
+
+
+#id_num = codecs.decode(id_num, 'hex').decode('ascii')
+
+#print(id_num)
+
 # if text_hex== "":
 #     print("is empty")
 # os.system(id_num)
-#
-#
+
+
 # write_card = 'yes N | nfc-mfultralight w mycardUltra.mfd'
 # os.system(write_card)
