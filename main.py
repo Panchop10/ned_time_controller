f6a45694a26dc125073ffb666e432c2d3835c5f3
ZeynepCAN-software <zeynepcan96@hotmail.com>
2019-07-31 12:51:47 +0100

    add comments to the code for reading the card

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import os
 import codecs
 
+#read card and save file in mdf
 read_card = 'nfc-mfultralight r mycardUltra.mfd'
 os.system(read_card)
 
+#convert mdf file to hex
 card_to_hex = 'xxd mycardUltra.mfd > mycardUltra.hex'
 os.system(card_to_hex)
+
+#read file
 f=open("mycardUltra.hex","r")
 first=f.readline()
 second=f.readline()
 
+#substring the second line, strip 0 and spaces
 id_num_hex=second[9:18]
 id_num_hex1=id_num_hex.replace(' ', '').rstrip('0')
-print(id_num_hex1)
-#id_num_hex1=id_num_hex.replace(' ', '').rstrip('0')
 
+#add 0 if lenght of the hex is odd
 if (len(id_num_hex1))%2 == 1:
-    id_num=id_num_hex1 + "0"
-#os.system(id_num)
-  id_num_hex1=id_num_hex1 + "0"
+    id_num_hex1=id_num_hex1 + "0"
 
+#decode hex into ascii
 id_num = codecs.decode(id_num_hex1, 'hex').decode('ascii')
 
 print(id_num)
