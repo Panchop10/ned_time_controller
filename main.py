b21d3640dcd5ae2adabffdf6dbb579797f70ec13
Sena Can <1997cansena@gmail.com>
2019-07-31 12:21:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ os.system(read_card)
 
 card_to_hex = 'xxd mycardUltra.mfd > mycardUltra.hex'
 os.system(card_to_hex)
-f=open("mycardUltra.hex","r")
-first=f.readline()
-Line=f.readline()
-id_num_hex=line.substring(9:17)
-print(id_num_hex)
 
 f=open("mycardUltra.hex","r")
 first=f.readline()
